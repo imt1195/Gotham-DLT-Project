e7fa4ab75b63025dd58e92c63dbeb0b92dcf368d
Heath Daniel <mr.heath.daniel@gmail.com>
2019-12-08 13:22:28 -0500

    1:22 Contract Update with Methods

--- contracts/MainCompetition.sol
+++ contracts/MainCompetition.sol
@@ -1,76 +1,53 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity >=0.4.21 <0.6.0;
 
-import "./ownable.sol";
+contract MainCompetition {
+    event StorageSet(string _message);
 
-contract MainCompetition is Ownable {
-
-    event NewCompetition (uint256 compId, string name);
-
-    // enum userType {Admin, Participant, Judge}
-    mapping(address => uint)  public addressToUserType;
-
-    // TO-DO: change to array? admin more than one Competition
-    // TO-DO: change state variables to not public
-    mapping(address => uint256) public adminToCompId;
-    mapping(uint256 => address) public compIdToAdmin;
+    uint public storedData;
 
     struct Competition {
         string name;
         uint256 totalPrize;
         bool isOpen;
+        // address[] participants;
     }
 
     Competition[] public competitions;
 
-    modifier isAdmin() {
-        require(addressToUserType[msg.sender] == 0, "The Sender is not an Admin of any Competition");
-        _;
+    function set(uint x) public {
+        storedData = x;
     }
 
-    modifier isParticipant() {
-        require(addressToUserType[msg.sender] == 1, "The Sender is not an Participant of any Competition");
-        _;
-    }
 
-    // modifier isParticipantOf (uint256 _CID, address _allegedParticipant) {
-    //     require(compIdToComp[_CID].participants[_allegedParticipant], "The proposed address is not a participant in that competition");
-    //     _;
-    // }
-
+    // Internal function for creating a competition
     function _createComp (string memory _name, uint256 _totalPrize) internal {
-        uint id = competitions.push(Competition(_name, _totalPrize,false));
-        compIdToAdmin[id] = msg.sender;
-        emit NewCompetition(id, _name);
-
+        // uint id =
+        competitions.push(Competition(_name, _totalPrize,false));
+        // compIdToAdmin[id] = msg.sender;
+        // emit NewCompetition(id, _name);
     }
 
-    function selectUserType (string memory _userType) public {
-        if (keccak256(abi.encodePacked(_userType)) == keccak256(abi.encodePacked("Admin"))) {
-            addressToUserType[msg.sender] = 1;
-        } else {
-            addressToUserType[msg.sender] = 2;
-        }
-    }
-
-    function createCompetition (string memory _name, uint256 _totalPrize) public payable isAdmin() {
-        require(msg.value == _totalPrize, "Message Value does not match prize amount.");
+    // As an admin, I can create a competition with a name and prize money
+    function createCompetition (string memory _name, uint256 _totalPrize) public {
+        // require(msg.value == _totalPrize, "Message Value does not match prize amount.");
         _createComp(_name, _totalPrize);
     }
 
-    // // Ends Competition -- Rescricted to Admins and Only Admins of this Competition
-    // function endCompetition (uint256 _CID) public isAdminOf(_CID) {
+    // As an admin, I can pull an array of competitions than I'm an admin of
+    function getMyCompetitions() public {
 
-    // }
+    }
+    // As an admin, I can end a competition and select the winning participant
+    function endCompetition (uint _compId, address _winningParticipant) public {
 
-    // function chooseWinner (uint256 _CID, address _winnerAddress) public isAdminOf(_CID) isParticipant()
-    // isParticipantOf(_CID, _winnerAddress) {
+    }
 
-    // }
+    // As a participant, I can sign up for a competition
+    function joinCompetition (uint _compId) public {
 
-    // function joinCompetition (uint256 _CID) public payable isCompetition(_CID) {
-    //     if (compIdToComp[_CID].fee > 0) {
-    //         require(msg.value >= compIdToComp[_CID].fee, "Value did not meet the competition fee");
-    //     }
-    // }
+    }
+
+
+
 
 }
--- test/mainCompetition.test.js
+++ test/mainCompetition.test.js
@@ -1,29 +1,15 @@
 const MainCompetition = artifacts.require("MainCompetition");
 
-// const compNames = ["Competition 1", "Competition 2"];
-// const prizeValues = [100,1000];
-// const userTypes = ["Admin", "Participant"];
-
 contract("MainCompetition", accounts => {
+  it("...should store the value 89.", async () => {
+    const MainCompetitionInstance = await MainCompetition.deployed();
 
-//   let [alice, bob] = accounts;
-//   let contractInstance;
-//   beforeEach(async () => {
-//     contractInstance = await MainCompetition.deployed();
-// });
+    // Set value of 89
+    await MainCompetitionInstance.set(89, { from: accounts[0] });
 
-it("...should set and check a user type.", async () => {
-  
-  contractInstance = await MainCompetition.deployed();
+    // Get stored value
+    const storedData = await MainCompetitionInstance.storedData.call();
 
-  // Set value
-  const result = await contractInstance.selectUserType("Admin", {from: accounts[0] });
-
-  // Get stored value
-  const userType = await contractInstance.addressToUserType[accounts[0]].call();
-
-  // // Check value
-  assert.equal(userType, 1, "The the user was set to an admin.");
-});
-
+    assert.equal(storedData, 89, "The value 89 was not stored.");
+  });
 });
